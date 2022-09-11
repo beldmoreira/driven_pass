1634f1b4c8618c794a6fa6e5797df19312c819b3
Isabela <isabela_cmoreira@hotmail.com>
2022-09-11 16:19:55 -0300

    feat: created error middleware

--- src/middlewares/errorHandlerMiddleware.ts
+++ src/middlewares/errorHandlerMiddleware.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 
-export default function errorHandler(
+export default function errorHandlerMiddleware(
   error,
   req: Request,
   res: Response,
