968e16b297cf50b15fb480d346c20380919824bd
Isabela <isabela_cmoreira@hotmail.com>
2022-09-11 16:19:36 -0300

    feat: created user controller

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-import app from "./app.js";
-
-const port = +process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server is up and running on port ${port}`);
-});
