0424da9c61c2e8d400973498d2f78516a838a524
Isabela <isabela_cmoreira@hotmail.com>
2022-09-12 17:25:55 -0300

    fix: changed again the functions and deleted unused features

--- src/util/crypt.ts
+++ src/util/crypt.ts
@@ -1,27 +1,11 @@
 import dotenv from "dotenv";
 dotenv.config({ path: ".env" });
-import * as bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
 import cryptr from "cryptr";
 
-export async function encryptPassword(password: string) {
-  const salt = await bcrypt.genSalt(10);
-  const hashPassword = await bcrypt.hash(password, salt);
-  return hashPassword;
+export function encrypt(value: string) {
+  return cryptr.encrypt(value);
 }
 
-export async function encrypt(data: string) {
-  const encryptedData = cryptr.encrypt(data);
-  return encryptedData;
-}
-
-export async function decrypt(encrypted: string) {
-  const decryptedData = cryptr.decrypt(encrypted);
-  return decryptedData;
-}
-
-export async function checkToken(token: string) {
-  const secretKey = process.env.JWT_SECRET;
-  const check = jwt.verify(token, secretKey);
-  return check;
+export function decrypt(encryptedValue: string) {
+  return cryptr.decrypt(encryptedValue);
 }
