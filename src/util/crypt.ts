b1c2b7919e51ec3df4fdf57be368867d93ccccc1
Isabela <isabela_cmoreira@hotmail.com>
2022-09-12 17:17:03 -0300

    fix: changed the functions in order to enable exports

--- src/util/crypt.ts
+++ src/util/crypt.ts
@@ -1,13 +1,27 @@
-import Cryptr from "cryptr";
 import dotenv from "dotenv";
 dotenv.config({ path: ".env" });
+import * as bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cryptr from "cryptr";
 
-const cryptr = new Cryptr(process.env.CRYPTR_SECRET);
-
-export function encrypt(value: string) {
-  return cryptr.encrypt(value);
+export async function encryptPassword(password: string) {
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(password, salt);
+  return hashPassword;
 }
 
-export function decrypt(encryptedValue: string) {
-  return cryptr.decrypt(encryptedValue);
+export async function encrypt(data: string) {
+  const encryptedData = cryptr.encrypt(data);
+  return encryptedData;
+}
+
+export async function decrypt(encrypted: string) {
+  const decryptedData = cryptr.decrypt(encrypted);
+  return decryptedData;
+}
+
+export async function checkToken(token: string) {
+  const secretKey = process.env.JWT_SECRET;
+  const check = jwt.verify(token, secretKey);
+  return check;
 }
